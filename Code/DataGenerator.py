minor bug with 1p accuracy calculation

Code/DataGenerator.py
@@ -171,8 +171,8 @@ for t in range(len(PlotTimes)):
         print('Done for case',k+1,'of',Cases,'!')
 
     ### Picking Random initial states for thermal case ###
-    if Regression == True: reglist = np.zeros((Cases*N_s,))
     if TDev != 0 and t == 1:
+        if Regression == True: reglist = np.zeros((Cases*N_s,))
         d1=RSE0List[0].shape[0]
         d2=RSE0List[0].shape[1]
         BigDSList = np.zeros((Cases*N_s,d1,d2))
@@ -195,9 +195,11 @@ for t in range(len(PlotTimes)):
     pd.DataFrame(PrePickedAS).to_csv('ExactTrajData_'+CaseString+'.csv',header=None,index=None)
     print('Done!')
     
-    if Regression == True:
-        reglist = reglist - min(reglist)
-        reglist = reglist/max(reglist)
+    if Regression == True and t == 1:
+        minr = min(reglist)
+        maxr = max(reglist)
+        reglist = reglist - minr
+        reglist = reglist/maxr
         reglist = 0.5*reglist
         reglist = reglist + 0.25
 
@@ -500,8 +502,12 @@ for t in range(len(PlotTimes)):
                     NN_output_tr=sess.run(Y_hat,feed_dict={X:x_train})
                     NN_output_va=sess.run(Y_hat,feed_dict={X:x_valid})                    
                     if Regression == True:
-                        acc_tr = np.mean(abs(NN_output_tr - y_train) < 0.1*y_train)
-                        acc_va = np.mean(abs(NN_output_va - y_valid) < 0.1*y_valid)
+                        temphat_tr = (NN_output_tr-1/4)*2*(maxr-minr)+minr
+                        temphat_va = (NN_output_va-1/4)*2*(maxr-minr)+minr
+                        temp_tr = (y_train-1/4)*2*(maxr-minr)+minr
+                        temp_va = (y_valid-1/4)*2*(maxr-minr)+minr
+                        acc_tr = np.mean(abs(temphat_tr - temp_tr) < 0.01*temp_tr)
+                        acc_va = np.mean(abs(temphat_va - temp_va) < 0.01*temp_va)
                     else:
                         predicted_class_tr = np.argmax(NN_output_tr, axis=1)
                         acc_tr = np.mean(predicted_class_tr == y_train)

Code/DataGenerator_Parameters.py
@@ -4,7 +4,7 @@ import numpy as np
 CaseString = 'RegStuff'     # String Associated with this run of the code
 GenerateData = True             # If False, code will read in old data
 N_Samples    = 200              # Number of examples to produce for training/validating
-Regression = False
+Regression = True
 
 ### Parameters of the quantum field ###
 sigma = 4.2                 # Detector Smearing
@@ -28,7 +28,7 @@ lam = lam/E0               # Normaalized Coupling Energy
 
 ### Measurement Options ###
 dt= 0.4             # Duration of each measurement window
-Tmin = 2.8            # Start of first measurement window
+Tmin = 2.4            # Start of first measurement window
 Tmax = 3.2          # End of last measurement window
 PlotTimes = list(np.linspace(Tmin,Tmax,int((Tmax-Tmin)/dt)+1)) # Linearly spaces measurement windows
 #PlotTimes += list(np.linspace(0.8,3.2,13)) # Linearly spaces measurement windows
@@ -108,7 +108,7 @@ f_valid = f_valid/fsum
 f_test  = f_test/fsum
 
 nH1 = 30                         # Number of neurons in the first hidden layer
-L2reg = 0.0001                        # L2 Regularizer
+L2reg = 0.001                        # L2 Regularizer
 learning_rate = 0.01           # Learning Rate
 N_epochs = 1000                   # Number of epoch to train over
 minibatch_size = 100             # Minibatch size